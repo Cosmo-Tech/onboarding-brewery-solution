6ab2d10cfa1a00c99f7fb9ad60cca10463301886
Vincent Carluer <vcarluer@gmail.com>
2024-08-02 16:47:48 +0200

    query results

--- API/Sample/set_env.sh
+++
@@ -1,8 +0,0 @@
-export USER_EMAIL=vincent.carluer@cosmotech.com
-export API_KEY=cosmotesta
-export BASE_URL=https://kubernetes.cosmotech.com/cosmotech-api/bahamut/dev
-export ORGANIZATION=o-d101zx7rp18r
-export SOLUTION=sol-09828mzjolkv
-export WORKSPACE=w-1ow0o4jr709x
-export SCENARIO=r-304vrk0e2y4l
-export RUN=run-vj0ex51gg1mo
---
+++ API/Sample/set_env_sample.sh
@@ -0,0 +1,8 @@
+export USER_EMAIL=changeme.email@company.com
+export API_KEY=my_api_key_changeme
+export BASE_URL=https://cosmotech-api-url-tenant-changeme
+export ORGANIZATION=o-changeme
+export SOLUTION=sol-changeme
+export WORKSPACE=w-changeme
+export SCENARIO=r-changeme
+export RUN=run-changeme
