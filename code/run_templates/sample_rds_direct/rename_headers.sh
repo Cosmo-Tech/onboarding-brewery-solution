60e46f480d244f4a868e4ecc628ab6df70af9482
Vincent Carluer <vcarluer@gmail.com>
2024-08-28 18:50:49 +0200

    find and sed space for output files

--- code/run_templates/sample_rds_direct/rename_headers.sh
+++ code/run_templates/sample_rds_direct/rename_headers.sh
@@ -1,1 +1,7 @@
-echo YOOOOOO
+if [ -z $CSM_OUTPUT_ABSOLUTE_PATH ]; then
+    echo "CSM_OUTPUT_ABSOLUTE_PATH is not set"
+    exit 1
+else
+    echo "CSM_OUTPUT_ABSOLUTE_PATH is set to $CSM_OUTPUT_ABSOLUTE_PATH"
+    find $CSM_OUTPUT_ABSOLUTE_PATH -type f -exec sed -i -e 's/ //g' {} \;
+fi
