665115b3abeedad1887ba0f804c7585de5715fed
Alexis Fossart <alexis.fossart@cosmotech.com>
2025-08-27 08:35:08 +0200

    Remove APi and code folder for pure simulator project

--- code/run_templates/orchestrator_tutorial/replace_scenariorun_data.sh
+++
@@ -1,4 +0,0 @@
-if [ -e "Simulation/Resource/scenariorun-data" ] || [ -L "Simulation/Resource/scenariorun-data" ]; then
-  mv Simulation/Resource/scenariorun-data Simulation/Resource/scenariorun-data.back
-fi 
-ln -s $(realpath $CSM_DATASET_ABSOLUTE_PATH) Simulation/Resource/scenariorun-data
--- code/run_templates/orchestrator_tutorial/restore_scenariorun_data.sh
+++
@@ -1,4 +0,0 @@
-if [ -e "Simulation/Resource/scenariorun-data.back" ] || [ -L "Simulation/Resource/scenariorun-data.back" ]; then
-  rm Simulation/Resource/scenariorun-data 
-  mv Simulation/Resource/scenariorun-data.back Simulation/Resource/scenariorun-data 
-fi
