1967fb4d0ddb9956f5a4fd39969f27e3fb9ec16e
Alexis Fossart <alexis.fossart@cosmotech.com>
2025-09-02 16:11:52 +0200

    correct simulator

--- code/run_templates/scripts/run_simulator.py
+++ code/run_templates/scripts/run_simulator.py
@@ -7,8 +7,8 @@ from get_logger import logger
 simulator = CosmoEngine.LoadSimulator("BusinessApp_Simulation")
 logger.info("Simulator loaded")
 
-if amqp_consumer_adress := os.environ.get(
-        "CSM_PROBES_MEASURES_TOPIC") is not None and "CSM_CONTROL_PLANE_TOPIC" in os.environ:
+if ((amqp_consumer_adress := os.environ.get("CSM_PROBES_MEASURES_TOPIC")) is not None
+        and "CSM_CONTROL_PLANE_TOPIC" in os.environ):
     # Remove local CSV consumers
     for consumer in simulator.GetConsumers():
         simulator.DestroyConsumer(consumer)
